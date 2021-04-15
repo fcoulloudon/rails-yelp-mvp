12333d4fdb9880dc1171dc0683b254baa0b6e130
Francois Coulloudon <fcoulloudon@gmail.com>
2021-04-15 19:04:52 +0200

    All rake OK

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,31 @@
 class RestaurantsController < ApplicationController
+  # before_action :restaurant, only: [:show, :index, :create, :new]
+  
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end    
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+    # params.require(:name).require(:category).require(:address).permit(:phone_number)
+  end
 end
